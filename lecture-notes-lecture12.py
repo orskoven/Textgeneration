cf2809d1d8343fb85584b15b96c36749248a8050
Simon Dieckmann Ørskov Beckmann <90264457+orskoven@users.noreply.github.com>
2024-10-25 13:39:39 +0200

    Create lecture-notes-lecture12.py

---
+++ lecture-notes-lecture12.py
@@ -0,0 +1,54 @@
+# Lecture 12 
+
+# walk through of peer review -> 
+
+      # Remember : kea name 
+      # Henrik explains : group names will enhance Henriks exam orchestration
+      # Methode : section (describe the methode framework ( no details included realted to the concrete data) 
+      # VERY importatn!!! - SORUCES -> Dont write a lecture book -> and remeber sources -> and reference yann lecun ex. on a deep neural network
+      # how to approach : ->> quick literature review before writing and article and scan and get an overview 
+      # Only reference a source a dont descrube methods concretly itself 
+      # INTRODUCTION :> is "setting the stage" -> how a motivation to read and reason
+      # WRITE A MATHIMATICAL equation to describe the linear regression (henrik aknowledge as clear and concis)
+      # WHITE SPACE 
+      # two column should help fit the right amount of text for the study 
+      # SENTENCES: beware of red
+      # overfitting > big gap between train and test data results in overfitting
+      # 
+      # Explicit make a judgement before making any statment .> compare etc. 
+      # We are allowed to have opinions in our conclusion at can make a judgement 
+
+
+      # How do we know our result are good? How a reason to state that 
+
+# My review 
+
+
+      # good title -> 
+      # gropp name -
+
+      # PAGES !!! dont make to many exam >>>>!!!
+      # source 
+      # good strucutre 
+      # discussion after conclusion 
+      # hypothesis -> merthods is analyse to concrete to the problem 
+      # to much analysis -> 
+      # Meythode >
+            #data cleaning -> 
+                # data preperation ->
+                  # feauture engineering 
+                        # modelling 
+
+      # first write the methode section : > i have made data preparation -> from using these sources -> Make thinngs repreducable 
+
+              # METHODE :_> should precisly carve out which methodes are being used
+    # START WITH THE METHODE SECTION
+    # it should be including all methodes -> and not too short and is well supported by sources
+    # New features -> explain why they are made and combining the feature -> WHY???? explaoimn !!!!!!!
+    # miss explanation for what really goes on -> analysis is missing and understand why things are used 
+      # show graf 
+      # conclusion should be sharper - > direct adressing the problem
+    # WE can use "we" we can use passive 
+
+    # methode reference algorithms -> correlations -> pearsons DNN deep neural network 
+      
